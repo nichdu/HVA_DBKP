48bf951d1319c9c3e5104cd2e20f2d042e1cc1c7
Tjark Saul <sauli1991@gmail.com>
2010-10-26 18:14:39 +0200

    Added GetTopDKP() function

--- getdkp/core.lua
+++ getdkp/core.lua
@@ -1,6 +1,6 @@
-local addonName, addonData = ...;
+﻿local addonName, addonData = ...;
 HVA_GetDKP = LibStub("AceAddon-3.0"):NewAddon("HVA_GetDKP", "AceConsole-3.0", "AceTimer-3.0", "AceEvent-3.0");
-local L = LibStub("AceLocale-3.0"):GetLocale("HVA_GetDKP");
+local L = LibStub("AceLocale-3.0"):GetLocale("HVA_GetDKP", true);
 
 do 
 	defaults = {
@@ -37,12 +37,33 @@ function HVA_GetDKP:IncWhisper(event, message, sender,...)
 end;
 
 function HVA_GetDKP:IncRaid(event, message, sender,...)
-	if (string.lower(message) == "listdkp" and (UnitIsRaidOfficer(sender) or UnitIsPartyLeader(sender))) then
+	if (string.lower(message) == "listdkp") then -- and (UnitIsRaidOfficer(sender) or UnitIsPartyLeader(sender))) then
 		local dkplist = self:GetTopDKP();
 		local msg = prefix .. L["List of DKP leaders"];
 		SendChatMessage(msg, "RAID");
 		for i=1,#dkplist do
-			SendChatMessage(prefix .. "1. " .. dkplist[i];
+			SendChatMessage(prefix .. i.. ". " .. dkplist[i]["Name"] .. ": " .. dkplist[i]["dkp"]);
 		end;
 	end;
+end;
+
+function HVA_GetDKP:GetTopDKP()
+	local names = {};
+	for i=1, GetNumRaidMembers() do
+		names[i] = select(1,UnitName("raid"..i));
+	end;
+	local dkp = {};
+	for name in pairs(names) do 
+		dkp[name] = HVA_DBKP:GetDKP(name);
+	end;
+	table.sort(dkp);
+	local top = {};
+	local i = 1;
+	for name, points in pairs(dkp) do
+		if (i > 5) then break end;
+		top[i]["Name"] = name;
+		top[i]["dkp"] = points;
+		i = i+1;
+	end;
+	return top;
 end;
